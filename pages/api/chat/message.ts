39fc3456d868e5fdf2dd9d8784b6b2ddb7b2cfa5
姚思谦 Yao Siqian <2229561981@qq.com>
2024-08-28 16:02:32 +0800

    fix: model

--- pages/api/chat/message.ts
+++ pages/api/chat/message.ts
@@ -13,14 +13,13 @@ export default async function handler(req: RequestParam, res: NextApiResponse) {
   const messages = JSON.parse(req.body);
   let response;
   try {
-    response = await fetch('https://api.openai.com/v1/chat/completions', {
+    response = await fetch('https://api.fastgpt.in/api/v1/chat/completions', {
       method: 'POST',
       headers: {
         Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        model: 'gpt-3.5-turbo',
         messages,
       }),
     });
