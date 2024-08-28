42972976a3b07e82650680c8703b5fee93391927
姚思谦 Yao Siqian <2229561981@qq.com>
2024-08-28 16:01:00 +0800

    style: GitHub link

--- components/GithubLink.tsx
+++ components/GithubLink.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import GithubLogo from '../public/assets/github.svg';
 export function GithubLink() {
   const handleClick = () => {
-    window?.open('https://github.com/shihui-huang/call-bob', '_blank')?.focus();
+    window?.open('https://github.com/YaoSiQian', '_blank')?.focus();
   };
   return (
     <div className="mx-1">
